b97bbd18849dbc10ba5b4e91f4b9374c03f09153
Joao Victor <joaov.scosta@outlook.com>
2020-04-28 19:35:07 -0300

    Remoção da função dotenv.config do index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import * as React from "react";
 import { render } from "react-dom";
 import App from "./App";
 
-dotenv.config();
-
 const rootElement = document.getElementById("root");
 render(<App />, rootElement);
 
---
+++ src/styles/bookList.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+const bookListStyles = makeStyles({
+  cardBook: {
+    maxWidth: "345px",
+  },
+  imageBook: {
+    height: "200px",
+  },
+});
+
+export default bookListStyles;
